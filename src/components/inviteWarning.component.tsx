583c8d5ccfc59294598659428510f98ac0365e3e
Najeeb Arshad <najeebarshad@hotmail.com>
2025-02-19 14:46:23 +0000

    feat: copy updates

--- src/components/inviteWarning.component.tsx
+++ src/components/inviteWarning.component.tsx
@@ -6,7 +6,7 @@ export function InviteWarning(): ReactNode {
   if (window.location.href.toLowerCase().includes("thatjeeb.github.io/recordstore")) {
     return (
       <p style={{ fontWeight: 600 }}>
-        This app is currently in invite only mode. You can login, but the app won&apos;t work unless you&apos;ve been given access. Please contact the developer
+        This app is currently in invite only mode. You can login, but trying to backup won&apos;t work unless you&apos;ve been given access. Please contact the developer
         to be invited.
       </p>
     );
