8b5f6eff96bed68e7f4393d574e6a2e18662fbdf
Najeeb Arshad <najeebarshad@hotmail.com>
2025-08-14 23:38:16 +0100

    add JSON file downloading

--- src/views/download/download.view.tsx
+++ src/views/download/download.view.tsx
@@ -1,5 +1,5 @@
 import React, { type ReactNode } from "react";
-import { DownloadDataAsTextButton } from "../../components";
+import { DownloadDataAsJsonButton, DownloadDataAsTextButton } from "../../components";
 import { AppClasses } from "../../styles/appClasses";
 
 export function Download(): ReactNode {
@@ -8,17 +8,17 @@ export function Download(): ReactNode {
       <p>
         Download backup data as <b>.txt</b> file:
       </p>
-      {/* <p className={AppClasses.SmallPrint}>This is the version most users will find useful.</p> */}
+      <p className={AppClasses.SmallPrint}>This is the version most users will find useful.</p>
       <div className={AppClasses.ButtonRow}>
         <DownloadDataAsTextButton>Download TXT File</DownloadDataAsTextButton>
       </div>
 
-      {/* <p>
+      <p>
         Download backup data as <b>.json</b> file:
       </p>
       <div className={AppClasses.ButtonRow}>
         <DownloadDataAsJsonButton>Download JSON File</DownloadDataAsJsonButton>
-      </div> */}
+      </div>
     </div>
   );
 }
